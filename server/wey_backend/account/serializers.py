b70a62f7dac184369219c87a02fb774d1867274e
ljh12138164 <3479261099@qq.com>
2025-03-23 20:06:09 +0800

    fix(social)修改字段

--- server/wey_backend/account/serializers.py
+++ server/wey_backend/account/serializers.py
@@ -27,7 +27,7 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ('id', 'name', 'email', 'friends_count', 'posts_count', 'get_avatar', 'bio', 'mbti_result', 'is_admin')
+        fields = ('id', 'name', 'email', 'friends_count', 'posts_count', 'get_avatar', 'bio', 'mbti_result', 'is_admin','date_joined')
     
     def get_mbti_result(self, obj):
         try:
